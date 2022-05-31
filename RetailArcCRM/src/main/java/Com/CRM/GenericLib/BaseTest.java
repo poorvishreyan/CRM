57ad2bbbb0b49eb11c146066e0166fb74149b935
poorvishreyan <106586181+poorvishreyan@users.noreply.github.com>
2022-05-31 15:29:43 +0530

    Update BaseTest.java

--- RetailArcCRM/src/main/java/Com/CRM/GenericLib/BaseTest.java
+++ RetailArcCRM/src/main/java/Com/CRM/GenericLib/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
+//written in global repo GitHub
 public class BaseTest implements IAutoConsts{
 	
 	public static WebDriver driver;
