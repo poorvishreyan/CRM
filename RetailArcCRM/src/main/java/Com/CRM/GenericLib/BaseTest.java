4b48798424c0c3e78b0845c6470b5b10b0e53225
miraganje manjunath <miraganje manjunath@Lenovo-PC>
2022-05-31 15:38:15 +0530

    BaseTest commit

--- RetailArcCRM/src/main/java/Com/CRM/GenericLib/BaseTest.java
+++ RetailArcCRM/src/main/java/Com/CRM/GenericLib/BaseTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 //written in global repo GitHub
+//written in local repo (Git)
 public class BaseTest implements IAutoConsts{
 	
 	public static WebDriver driver;
